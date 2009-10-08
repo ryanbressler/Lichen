e5ffcfeb970371f6c45dd81caf084bc053e9d1bc
Ryan Bressler <rbressle@platypus.systemsbiology.net>
2009-10-08 12:49:34 -0700

    Documentation woot

--- bionetwork.js
+++ bionetwork.js
@@ -27,15 +27,6 @@ if(!org) {
 }
 
 
-// ---------------------------------------------------------------------------------------------------------------------
-// - BioNetwork
-// -
-// -   Description: Draws a gene expression style heatmap using Canvas
-// -   Author: dburdick
-// -   Version: 1.0
-// -
-// ---------------------------------------------------------------------------------------------------------------------
-
 org.systemsbiology.visualization.BioNetwork = Class.create({
 
 
@@ -61,6 +52,16 @@ org.systemsbiology.visualization.BioNetwork = Class.create({
     	this.visindex = isbSWFvisualizations["SWFcount"];
     	this.SWFid= "bionetwork"+this.visindex;
     	isbSWFvisualizations[this.SWFid] = this;
+    	this.flashLoading = true;
+    	var readyFnc = function()
+    	{
+    		this.flashLoading = false;
+    	};
+    	//global function could be bad?
+    	this.flashReady = readyFnc.bind(this);
+    	//hidden input or div to get around ie 6 foolishness with scope/noscope objects
+    	var embedString = "<div></div><object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0\" id=\""+this.SWFid+"\" width=\"100%\" height=\"100%\"><param name=\"movie\" value=\"bionetwork.swf\" /><param name=\"quality\" value=\"high\" /><param name=\"wmode\" value=\"opaque\"><param name=\"flashvars\" value=\"flashvarsId="+this.SWFid+"\" /><param name=\"bgcolor\" value=\"#FFFFFF\" /> <param name=\"allowScriptAccess\" value=\"sameDomain\" /><embed src=\"bionetwork.swf\" quality=\"high\" wmode=\"opaque\" bgcolor=\"#FFFFFF\" width=\"100%\" height=\"100%\" name=\""+this.SWFid+"\" flashvars=\"flashvarsId="+this.SWFid+"\" align=\"middle\" play=\"true\" loop=\"false\" quality=\"high\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\"></embed></object>";
+		this.containerElement.innerHTML=embedString;
     	
     	},
 
@@ -70,59 +71,32 @@ org.systemsbiology.visualization.BioNetwork = Class.create({
 
     // Parameter options is a name/value map of options.
 
-
     draw: function(data, options) {
 
-    	this.flashLoading = true;
-    	var readyFnc = function()
-    	{
-    		this.flashLoading = false;
-    	};
-    	//global function could be bad?
-    	this.flashReady = readyFnc.bind(this);
-    	//hidden input or div to get around ie 6 foolishness with scope/noscope objects
-    	var embedString = "<div></div><object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0\" id=\""+this.SWFid+"\" width=\"100%\" height=\"100%\"><param name=\"movie\" value=\"bionetwork.swf\" /><param name=\"quality\" value=\"high\" /><param name=\"flashvars\" value=\"flashvarsId="+this.SWFid+"\" /><param name=\"bgcolor\" value=\"#FFFFFF\" /> <param name=\"allowScriptAccess\" value=\"sameDomain\" /><embed src=\"bionetwork.swf\" quality=\"high\" bgcolor=\"#FFFFFF\" width=\"100%\" height=\"100%\" name=\""+this.SWFid+"\" flashvars=\"flashvarsId="+this.SWFid+"\" align=\"middle\" play=\"true\" loop=\"false\" quality=\"high\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\"></embed></object>";
-		this.containerElement.innerHTML=embedString;
-		//this.containerElement.innerHTML ="<object id=\"bioheatmap\" codebase=\"http://fpdownload.macromedia.com/get/flashplayer/current/swflash.cab\"> <param name=\"movie\" value=\"bioheatmap.swf\" /> <param name=\"quality\" value=\"high\" /> <param name=\"bgcolor\" value=\"#FFFFFF\" /> <param name=\"allowScriptAccess\" value=\"sameDomain\" /> <embed src=\"bioheatmap.swf\" quality=\"high\" bgcolor=\"#FFFFFF\" name=\"bioheatmap\" align=\"middle\" play=\"true\" loop=\"false\" quality=\"high\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\"> </embed> </object>"
-		
+    	
 		//wait for swf to be ready...is this necesairy?
-		//log("dataparam");
-		var dataparam = this.buildDataParam(data);
-		//var optionsparam = this.buildDataParam(options);
+		var dataparam = data ==''?'' : this.buildDataParam(data);
 		var optionsparam = {};
-		for (key in options){
-			if (typeof(options[key])=='object'){
-				optionsparam[key] = this.buildDataParam(options[key]);
+		for (opt in options){
+			if (typeof(options[opt])=='object'){
+				//inner loop for checking table
+				//if (options[key]['tqx']){
+					if (options[opt]['getNumberOfRows']){
+						optionsparam[opt] = this.buildDataParam(options[opt]);
+					} 
+				else{
+					optionsparam[opt]=options[opt];
+				}	
 			}
 			else{
-				optionsparam[key] = options[key];
+				optionsparam[opt] = options[opt];
 			}
-		}
-		
+		}	
     myPe = new PeriodicalExecuter(this.swfPoll.bind(this,dataparam,optionsparam), .01);
 	//myPe = new PeriodicalExecuter(this.swfPoll.bind(this,dataparam,optionsparam),.01);
 		//alert(Object.toJSON(data));
     },
     
-    update_data : function(data,options) {
-		swf = this.getSWF(this.SWFid);
-        if (!this.flashLoading) {
-           //swf.update_data("hello");
-           //this.swfPoll.bind(this,data,options);
-           //log("viz_id"+ this.SWFid);
-           //log(data);
-           //log(options);
-           //log(swf);
-           var dataparam = this.buildDataParam(data);
-           var optionsparam = this.buildDataParam(options['attributes']);
-           //this.swf.draw(data,options);
-		  swf.redraw(Object.toJSON(dataparam),Object.toJSON({attributes: optionsparam, center: options['center'], data_format: "google"}));
-          
-           //this.swfPoll.bind(this,dataparam, {attributes: optionsparam, center: '3630', data_format: "static"});
-           // myPe = new PeriodicalExecuter(this.swfPoll.bind(this,data,options), .01);
-		   //log("js update_data");
-		}
-    },
     
     isEmpty : function(object) {
 		for (var i in object) { return false; }
@@ -130,15 +104,11 @@ org.systemsbiology.visualization.BioNetwork = Class.create({
 	},
     
    buildDataParam : function(dataTable){
- 
     	var dataParam = {cols:[], rows:[]};
-    	
     	for (var coli=0;coli<dataTable.getNumberOfColumns();coli++) 
 		{
-			dataParam.cols[coli]={id: dataTable.getColumnId(coli), label: dataTable.getColumnLabel(coli), type: 'string'};
-			
+			dataParam.cols[coli]={id: dataTable.getColumnId(coli), label: dataTable.getColumnLabel(coli), type: 'string'};	
 		}
-		
 		for (var rowi=0;rowi<dataTable.getNumberOfRows();rowi++) 
 		{
 			dataParam.rows[rowi]={};
@@ -149,10 +119,8 @@ org.systemsbiology.visualization.BioNetwork = Class.create({
 			dataParam.rows[rowi].c[coli]={v:dataTable.getValue(rowi,coli)};
 			if(dataTable.getFormattedValue(rowi,coli))
 				dataParam.rows[rowi].c[coli].f=dataTable.getFormattedValue(rowi,coli);
-			
 			}
-		}
-    	
+		} 	
     	return dataParam;
     },
       
@@ -161,8 +129,7 @@ org.systemsbiology.visualization.BioNetwork = Class.create({
   		if (!this.flashLoading){
   		pe.stop();
   		var swf = this.getSWF(this.SWFid);
-
-		swf.draw(Object.toJSON(data),Object.toJSON(options));
+		swf.draw(data==""?"":Object.toJSON(data),Object.toJSON(options));
 
     	}
     },
@@ -182,6 +149,7 @@ org.systemsbiology.visualization.BioNetwork = Class.create({
     // set's the current selected rows, cols or cells
 
     setSelection: function(selection, append) {
+    	
     	if(append!=true)
     	{
     		this._selectedNodes=new Array();
@@ -193,7 +161,7 @@ org.systemsbiology.visualization.BioNetwork = Class.create({
 			{
 				this._selectedNodes.push(selection[rowi]);
 			}
-			else
+			elsehttp://rbressle.gdxbase.org/images//bullet_red_small.gif
 			{
 				this._selected.push(selection[rowi]);
 			}
@@ -202,6 +170,19 @@ org.systemsbiology.visualization.BioNetwork = Class.create({
         swf.selectionSetViaJS(Object.toJSON(this._selected.concat(this._selectedNodes)));
 
     },
+    
+    add_edge_from_to : function(n1,n2){
+    	this.getSWF(this.SWFid).add_edge_from_to(n1,n2);
+    },
+    
+    remove_node_by_name : function(n1){
+    	this.getSWF(this.SWFid).remove_node_by_name(n1);
+    },
+    
+    remove_edge_from_to : function(n1,n2){
+    	this.getSWF(this.SWFid).remove_edge_from_to(n1,n2);
+    },
+    
    
     
     	// Gets a reference to the specified SWF file by checking which browser is
